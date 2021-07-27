1ea6c0c4a43a0771abaaefac1b647c6baabeb6b9
MuhammadHasanUsmani <hasanusmani104@gmail.com>
2021-07-27 06:32:25 -0700

    new

---
+++ Assets/Scripts/Fruit.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fruit : MonoBehaviour
+{
+    Rigidbody rb;
+    Rigidbody Childrb;
+    Collider ChildCollider;
+    Collider SecondCollider;
+    public GameObject pear;
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        pear = GetComponentInChildren<GameObject>();
+    }
+    //private void OnTriggerEnter (Collision collision)
+    //{
+    //    if (collision.collider.tag == "Knife")
+    //    {
+    //        pear = pear.transform.GetChild(0).gameObject;
+    //        pear.transform.parent=null;
+    //        rb.useGravity = true;
+    //        transform.parent = null;
+    //    }
+
+    //}
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Knife")
+        {
+            Childrb = pear.transform.GetChild(1).gameObject.GetComponent<Rigidbody>();
+            Childrb.useGravity = true;
+            pear = pear.transform.GetChild(0).gameObject;
+            Childrb = pear.GetComponent<Rigidbody>();
+            Childrb.useGravity = true;
+            rb.useGravity = true;
+            DelayCollider(pear);
+            
+        }
+
+    }
+    public void ColliderOn(GameObject FirstChild, GameObject SecondChild)
+    {
+        SecondCollider = FirstChild.GetComponent<MeshCollider>();
+        SecondCollider.enabled = true;
+        ChildCollider = SecondChild.GetComponent<SphereCollider>();
+        ChildCollider.enabled = true;
+    }
+    private IEnumerator DelayCollider(GameObject pear)
+    {
+        yield return new WaitForSeconds(0.2f);
+        ColliderOn(transform.GetChild(0).gameObject, transform.GetChild(1).gameObject);
+        pear.transform.parent = null;
+        transform.parent = null;
+    }
+}
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -12,6 +12,13 @@ public class GameController : MonoBehaviour
     //we can get this instance from other scripts very easily
     public static GameController Instance { get; private set; }
 
+    [Header("LogPrefebs")]
+    public List<GameObject> logPrefebs = new List<GameObject>();
+    [SerializeField] GameObject SpawnyLog;
+
+    //logPrefebs  currentlog;
+    [SerializeField]   Vector3 logSpawnPosition = new Vector3 () ;
+
     [SerializeField]
     private int knifeCount;
 
@@ -35,11 +42,14 @@ public class GameController : MonoBehaviour
 
     private void Start()
     {
+        knifeCount = Random.Range(1, 3);
         //update the UI as soon as the game starts
         GameUI.SetInitialDisplayedKnifeCount(knifeCount);
         //also spawn the first knife
         SpawnKnife();
+        SpawnLog();
     }
+    
 
     //this will be called from KnifeScript
     public void OnSuccessfulKnifeHit()
@@ -61,6 +71,19 @@ public class GameController : MonoBehaviour
         Instantiate(knifeObject, knifeSpawnPosition, Quaternion.identity);
     }
 
+    private void SpawnLog()
+    {
+
+        
+            print("firstlog");
+
+        
+
+        Instantiate(SpawnyLog, logSpawnPosition, Quaternion.identity);
+            
+        
+    }
+
     //the public method for starting game over
     public void StartGameOverSequence(bool win)
     {
@@ -78,17 +101,42 @@ public class GameController : MonoBehaviour
             yield return new WaitForSecondsRealtime(0.3f);
             //Feel free to set different values for knife count and log's rotation pattern
             //instead of just restarting. This would make it feel like a new, harder level.
-            RestartGame();
+            NewLog();
+           
         }
         else
         {
             GameUI.ShowRestartButton();
         }
     }
+    public void SpawnLogMotor()
+    {
+        if (knifeCount <= 0)
+        {
+            GameUI.SetInitialDisplayedKnifeCount(knifeCount);
+            //Destroy(SpawnyLog);
+            //SpawnyLog.GetComponent<Renderer>().enabled = false;
+            knifeCount = Random.Range(1, 3);
+            //update the UI as soon as the game starts
+            GameUI.SetInitialDisplayedKnifeCount(knifeCount);
 
-    public void RestartGame()
+            for (int i = 0; i < logPrefebs.Count; i++)
+            {
+               
+                Instantiate(logPrefebs[i], logSpawnPosition, Quaternion.identity);
+                print("newlog");
+            }
+        }
+    }
+    public void NewLog()
     {
         //restart the scene by reloading the currently active scene
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex, LoadSceneMode.Single);
+        SpawnLogMotor();
+        SpawnKnife();
+    }
+    public void RestartGame()
+    {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex, LoadSceneMode.Single);
     }
 }
